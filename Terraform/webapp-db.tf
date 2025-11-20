cce5d0f8e8f14a5d50a7e101a2c02f83cab7b87f
Timur Makimov <megapers@gmail.com>
2025-11-19 23:26:43 -0800

    refactor: Terraform files update

--- Terraform/webapp-db.tf
+++ Terraform/webapp-db.tf
@@ -3,26 +3,25 @@ resource "azurerm_resource_group" "rg" {
   location = var.location
 }
 
-resource "azurerm_app_service_plan" "plan" {
+resource "azurerm_service_plan" "plan" {
   name                = "example-appserviceplan"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-
-  sku {
-    tier = "Standard"
-    size = "S1"
-  }
+  os_type             = "Windows"
+  sku_name            = "F1"
 }
 
-resource "azurerm_app_service" "app-service" {
+resource "azurerm_windows_web_app" "app-service" {
   name                = var.app-service-name
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  app_service_plan_id = azurerm_app_service_plan.plan.id
+  service_plan_id     = azurerm_service_plan.plan.id
 
   site_config {
-    dotnet_framework_version = "v4.0"
-    scm_type                 = "LocalGit"
+    application_stack {
+      current_stack  = "dotnet"
+      dotnet_version = "v8.0"
+    }
   }
 
   app_settings = {
@@ -32,24 +31,24 @@ resource "azurerm_app_service" "app-service" {
   connection_string {
     name  = "Database"
     type  = "SQLServer"
-    value = "Server=tcp:${azurerm_sql_server.sqldb.fully_qualified_domain_name} Database=${azurerm_sql_database.db.name};User ID=${azurerm_sql_server.sqldb.administrator_login};Password=${azurerm_sql_server.sqldb.administrator_login_password};Trusted_Connection=False;Encrypt=True;"
+    value = "Server=tcp:${azurerm_mssql_server.sqldb.fully_qualified_domain_name} Database=${azurerm_mssql_database.db.name};User ID=${azurerm_mssql_server.sqldb.administrator_login};Password=${azurerm_mssql_server.sqldb.administrator_login_password};Trusted_Connection=False;Encrypt=True;"
   }
 }
 
-resource "azurerm_sql_server" "sqldb" {
+resource "azurerm_mssql_server" "sqldb" {
   name                         = "terraform-sqlserver"
   resource_group_name          = azurerm_resource_group.rg.name
   location                     = azurerm_resource_group.rg.location
   version                      = "12.0"
   administrator_login          = "houssem"
   administrator_login_password = "4-v3ry-53cr37-p455w0rd"
+  minimum_tls_version          = "1.2"
 }
 
-resource "azurerm_sql_database" "db" {
-  name                = "terraform-sqldatabase"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-  server_name         = azurerm_sql_server.sqldb.name
+resource "azurerm_mssql_database" "db" {
+  name      = "terraform-sqldatabase"
+  server_id = azurerm_mssql_server.sqldb.id
+  sku_name  = "Basic"
 
   tags = {
     environment = "production"
